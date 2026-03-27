255146508efb8484b14ba3b87f6a952e665ff4ff
Ahmet Furkan Kocabas <ahmetfurkan.kocabas@agu.edu.tr>
2026-03-27 17:52:51 +0300

    Cache read implemented.

--- cache/Cache.sv
+++ cache/Cache.sv
@@ -7,33 +7,57 @@ module Cache (
     input logic     read_enable,
     input address_t address,
 
-    output data_t read_data
+    output word_t read_data,
+    output logic  is_Hit
 );
 
   // Internal Blocks:   {
-  cacheblock_t data      [NUM_OF_CACHE_LINES];
-  offset_t     offset    [NUM_OF_CACHE_LINES];
-  tag_t        tag       [NUM_OF_CACHE_LINES];
+  cacheblock_t  data        [NUM_OF_CACHE_LINES];
+  offset_t      offset      [NUM_OF_CACHE_LINES];
+  tag_t         tag         [NUM_OF_CACHE_LINES];
+  valid_t       valid       [NUM_OF_CACHE_LINES];
 
-  offset_t     offset_in;
-  tag_t        tag_in;
-  index_t      index_in;
+  offset_t      offset_in;
+  tag_t         tag_in;
+  index_t       index_in;
+
+  cacheblock_t  data_block;
+
+  word_select_t word_select;
   // ------------------ }
 
   // Assignments:    {
-  assign offset_in = address[];
-  assign tag_in    = address[];
-  assign index_in  = address[];
+  assign offset_in   = address[OFFSET_MSB:OFFSET_LSB];
+  assign tag_in      = address[TAG_MSB:TAG_LSB];
+  assign index_in    = address[INDEX_MSB:INDEX_LSB];
+  assign data_block  = data[index_in];
+  assign word_select = offset_in[WORD_SELECT_MSB:WORD_SELECT_LSB];
+
   // ----------------}
 
 
-  always_ff @(posedge clock) begin : read_data
+  always_ff @(posedge clock) begin : reading
+    // Reset  logic goes here
     if (res) begin
-
+      is_Hit <= '0;
+      read_data <= '0;
+      valid <= '{default: 0};
     end else begin
-
+      if (read_enable) begin
+        if (valid[index_in] && (tag[index_in] == tag_in)) begin  // Cache hit
+          is_Hit <= 1;
+          /*
+          TODO: read_data is a 32 bit word while data[index] is a cache line of 512 bits.
+            */
+          read_data <= data_block[word_select*WORD_SIZE_IN_BITS+:WORD_SIZE_IN_BITS];
+        end else begin  // Cache miss.
+          is_Hit <= 0;
+        end
+        // Clear output signals
+      end else begin
+        is_Hit <= 0;
+      end
     end
-
   end
 
 endmodule
--- cache/CachePackage.sv
+++ cache/CachePackage.sv
@@ -5,6 +5,7 @@ package CachePackage;
   parameter int CPU_DATA_WIDTH_IN_BYTES = CPU_DATA_WIDTH_IN_BITS / 8;
 
   parameter int WORD_SIZE_IN_BYTES = CPU_DATA_WIDTH_IN_BYTES;
+  parameter int WORD_SIZE_IN_BITS = WORD_SIZE_IN_BYTES * 8;
 
   parameter int CACHE_SIZE_IN_BYTES = 4 * 1024;
   parameter int CACHE_BLOCK_SIZE_IN_BYTES = 64;
@@ -20,7 +21,9 @@ package CachePackage;
   parameter int TAG_BITS = ADDRESS_SPACE_IN_BITS - INDEX_BITS - OFFSET_BITS;
 
   parameter int WORDS_PER_CACHE_BLOCK = CACHE_BLOCK_SIZE_IN_BYTES / WORD_SIZE_IN_BYTES;
-  parameter int WORD_OFFSET_BITS = $clog2(WORDS_PER_CACHE_BLOCK);
+  parameter int WORD_SELECT_BITS = $clog2(WORDS_PER_CACHE_BLOCK);
+
+  parameter int BYTE_SELECT_BITS = $clog2(WORD_SIZE_IN_BYTES);
 
   // Type Definitions
   typedef logic [CACHE_BLOCK_SIZE_IN_BITS - 1:0] cacheblock_t;  // 512 bits at the moment.
@@ -29,11 +32,24 @@ package CachePackage;
   typedef logic [OFFSET_BITS -1:0] offset_t;
   typedef logic [INDEX_BITS -1:0] index_t;
   typedef logic [TAG_BITS -1:0] tag_t;
+  typedef logic valid_t;
+  typedef logic [WORD_SIZE_IN_BITS-1:0] word_t;  // 32 bits at the moment.
+  typedef logic [WORD_SELECT_BITS-1:0] word_select_t;
 
-  /*
-   TODO: Find a better typedefinition for the data_t. This is the data being read by the
-   CPU. There is currently no byte selection.
-  */
-  typedef logic [CPU_DATA_WIDTH_IN_BYTES-1:0] data_t;  // 32 bits at the moment.
+  // LSB and MSB of fields in an memory address
+  parameter int OFFSET_LSB = 0;
+  parameter int OFFSET_MSB = OFFSET_BITS - 1;
 
+  parameter int INDEX_LSB = OFFSET_BITS;
+  parameter int INDEX_MSB = (INDEX_BITS + OFFSET_BITS) - 1;
+
+  parameter int TAG_LSB = (INDEX_BITS + OFFSET_BITS);
+  parameter int TAG_MSB = (TAG_BITS + INDEX_BITS + OFFSET_BITS) - 1;
+
+  // Word and byte select bits are inside the offset bits
+  parameter int WORD_SELECT_LSB = BYTE_SELECT_BITS;
+  parameter int WORD_SELECT_MSB = (WORD_SELECT_LSB + WORD_SELECT_BITS) - 1;
+
+  parameter int BYTE_SELECT_LSB = 0;
+  parameter int BYTE_SELECT_MSB = BYTE_SELECT_BITS - 1;
 endpackage
