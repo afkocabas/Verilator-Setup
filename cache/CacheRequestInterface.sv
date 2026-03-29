1d10737efed3e0cd21013c1b4f07aece15d5dbb6
Ahmet Furkan Kocabas <ahmetfurkan.kocabas@agu.edu.tr>
2026-03-29 10:35:46 +0300

    Cache realted interfaces.

---
+++ cache/CacheRequestInterface.sv
@@ -0,0 +1,52 @@
+import CachePackage::*;
+
+interface cache_req_if (
+    input clk
+);
+
+  address_t wr_addr;
+  address_t rd_addr;
+
+  logic res;
+
+  logic write_en;
+  logic read_en;
+
+  logic is_hit;
+
+  word_t wr_data;
+  word_t rd_data;
+
+
+  modport master(
+      output res,
+
+      output write_en,
+      output read_en,
+
+      output wr_addr,
+      output rd_addr,
+
+      output wr_data,
+
+      input rd_data,
+      input is_hit
+  );
+
+  modport slave(
+      input res,
+
+      input write_en,
+      input read_en,
+
+      input wr_addr,
+      input rd_addr,
+
+      input wr_data,
+
+      output rd_data,
+      output is_hit
+  );
+
+
+endinterface
---
+++ cache/CacheStorageInterface.sv
@@ -0,0 +1,55 @@
+import CachePackage::*;
+
+interface cache_storage_if (
+    input clk
+);
+
+  logic        res;
+
+  logic        wr_en;
+  tag_t        wr_tag;
+  valid_t      wr_valid;
+  cacheblock_t wr_block;
+  index_t      wr_idx;
+
+  index_t      rd_idx;
+
+  tag_t        tag;
+  cacheblock_t block;
+  valid_t      valid;
+
+
+  modport master(
+      output res,
+      output wr_en,
+
+      output wr_tag,
+      output wr_valid,
+      output wr_block,
+      output wr_idx,
+      output rd_idx,
+
+      input tag,
+      input block,
+      input valid
+
+  );
+
+  modport slave(
+      input res,
+      input wr_en,
+
+      input wr_tag,
+      input wr_valid,
+      input wr_block,
+      input wr_idx,
+      input rd_idx,
+
+      output tag,
+      output block,
+      output valid
+
+  );
+
+endinterface
+
